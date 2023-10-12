92ae6834d6df8e20b94a7cd666a087a4523c9bbf
ErikEndler <eng.erik.endler@gmail.com>
2023-10-12 17:58:09 -0300

    feat(EVV-3): add modal dinamic component

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import App from './App.vue'
 import router from './router'
 import { createPinia } from 'pinia'
 import i18n from './i18n'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap'
 
 import './assets/styles/app.scss'
 
