fe572d387fa9072da7b8983120ef8ad94eaee73c
ErikEndler <eng.erik.endler@gmail.com>
2024-01-09 19:33:48 -0300

    feat(EVV-5): enabling for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ import VueI18nPlugin from '@intlify/unplugin-vue-i18n/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'erik-vue-vite',
   plugins: [
     vue(),
     VueI18nPlugin({
