2017572e71bc2b105a525a4d71f74c7df63c0157
ErikEndler <eng.erik.endler@gmail.com>
2024-01-10 00:58:26 -0300

    fix(EVV-5): adjustment router to gh-pages

--- src/router/index.ts
+++ src/router/index.ts
@@ -5,12 +5,12 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.VITE_BASE_URL),
   routes: [
     {
-      path: '/',
+      path: '/erik-vue-vite',
       name: 'Home',
       component: HomeView
     },
     {
-      path: '/simple-form',
+      path: '/erik-vue-vite/simple-form',
       name: 'SimpleForm',
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
@@ -18,7 +18,7 @@ const router = createRouter({
       component: () => import(/* webpackChunkName: "SimpleForm" */ '../views/SimpleForm.vue')
     },
     {
-      path: '/tip-calculator',
+      path: '/erik-vue-vite/tip-calculator',
       name: 'TipCalculator',
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
@@ -26,13 +26,14 @@ const router = createRouter({
       component: () => import(/* webpackChunkName: "TipCalculator" */ '../views/TipCalculator.vue')
     },
     {
-      path: '/modals',
+      path: '/erik-vue-vite/modals',
       name: 'Modals',
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import(/* webpackChunkName: "ModalsView" */ '../views/ModalsView.vue')
-    }
+    },
+    { path: '/:pathMatch(.*)*', redirect: '/erik-vue-vite' }
   ]
 })
 
