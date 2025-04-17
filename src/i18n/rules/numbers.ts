38e91939cb2239e9b9245525c4e35fdb2d9465c7
ErikEndler <eng.erik.endler@gmail.com>
2025-04-17 17:52:42 -0300

    fix(EVV-6): fix deploy

--- src/i18n/rules/numbers.ts
+++ src/i18n/rules/numbers.ts
@@ -1,14 +1,21 @@
-export default {
+export interface CurrencyFormatOptions {
+  style: 'currency'
+  currency: string
+}
+
+const numberFormats = {
   en: {
     currencyFormat: {
       style: 'currency',
       currency: 'USD'
-    }
+    } as CurrencyFormatOptions
   },
   pt: {
     currencyFormat: {
       style: 'currency',
       currency: 'BRL'
-    }
+    } as CurrencyFormatOptions
   }
 }
+
+export default numberFormats
